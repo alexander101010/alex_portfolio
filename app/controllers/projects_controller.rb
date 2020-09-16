f94f77325ad159126d5ff5d9aadd7763c2af8326
Alex Botwinick <alexbotwinick@gmail.com>
2020-09-16 12:12:48 +0200

    projects controller and resulting routes

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,28 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :url)
+  end
+end
